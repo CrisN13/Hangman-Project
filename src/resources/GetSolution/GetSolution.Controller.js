19a5ae2e39df1c2b80778802e6b55425c9bffcee
Cristian <cristian.navarro@elaniin.com>
2021-09-30 12:30:11 -0600

    Solución persistencia de datos

--- src/resources/GetSolution/GetSolution.Controller.js
+++ src/resources/GetSolution/GetSolution.Controller.js
@@ -3,8 +3,6 @@ const { GetWord } = require('../Data/Game')
 
 //Método que muestra la solución del juego
 module.exports.GuessWordSolutionController = (req, res) => {
-    //let word =  dataGame.word
-    let wordH = GetWord()
-    console.log(wordH)
-    return res.send("<body bgcolor='slateblue' style='color: white'>La palabra correcta es: <b>" + wordH + "</b></body>")
+    let word = GetWord()
+    return res.send("<body bgcolor='slateblue' style='color: white'>La palabra correcta es: <b>" + word + "</b></body>")
 }
